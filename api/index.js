9dc958fd7eea0438215f1068de95dee224a3e492
Mohitpuri Goswami <113592385+mohit5723@users.noreply.github.com>
2024-04-20 00:40:17 +0530

    vercel updates

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const Todo = require('./models/todo.models.js');
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors({ credentials: true, origin: 'https://todo-pal.vercel.app/', methods:["POST","GET"], }));
 const uri =
 	'mongodb+srv://mohitgoswami379:H30y9LOv4RskaO6p@cluster0.dohlaol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(uri);
